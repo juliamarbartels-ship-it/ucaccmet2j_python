49903bd77be16adc4038c1a0a20c0b172966c877
Julia Bartels <julia.mar.bartels@gmail.com>
2026-01-13 14:35:52 +0100

    access stations data from csv file

--- assignment_4_script.py
+++ assignment_4_script.py
@@ -1,102 +1,68 @@
 
-# This script analyzes rain over time in Seattle. 
-# The output is a json file including the station, state, 
-# total and relative monthly precipitation, and total and monthly
-# yearly precipitation per State
-
-# Part 1: Monthly Precipitation
+# This script analyzes rain over time in Seattle. The output is a json file including the station, state, 
+# total and relative monthly precipitation, and total and monthly yearly precipitation per State
 
 # import packages
 import json
 
 # load in files
-# with open("stations.csv", encoding= "utf-8") as file:  
-#     stations = file.read()
 with open('precipitation.json', encoding='utf-8') as file:
     precipitation_data = json.load(file)
+with open("stations.csv", encoding= "utf-8") as file:  
+    first_line = file.readline()        # disregard line of column names
+    stations = file.readlines()         
 
-# 0. loop over each station
-# 0.1 make dictionary of all stations (key: city, value: station name)
-stations_dict = {
-    "Cincinnati": {
-        "state": "OH",
-        "station": "GHCND:USW00093814"}, 
-    "Seattle": {
-        "state": "WA",
-        "station": "GHCND:US1WAKG0038"}, 
-    "Maui": {
-        "state": "HI",
-        "station": "GHCND:USC00513317"},
-    "San Diego": {
-        "state": "CA",
-        "station": "GHCND:US1CASD0032"}
-}
-print(stations_dict["Cincinnati"])
-# initialize dictionary for json output file
-precipitation_summary = {}
+# 1. make dictionary of stations to summarize results
+stations_dict = {}
+for line in stations:      # for each line, add information into dictionary so key = city, value = dictionary (with keys for station and state)
+    city, state, station = line.split(",")
+    stations_dict[city] = {"state": state, "station": station.strip()}
 
-# 0.2 loop over all cities
+# 2. loop over all cities to analyze each
 for city in stations_dict:
-    #access station and state of each city
+
+    # 2.1 access station and state of current city
     city_dict = stations_dict[city]
     station = city_dict["station"]
     state = city_dict["state"]
 
-    # select measurements for Seattle
-    station_precipitation = []
+    # 2.2 select measurements for current city
+    city_precipitation = []
     for observation in precipitation_data:
         if observation["station"] == station:
-            station_precipitation.append(observation)
+            city_precipitation.append(observation)
 
-    # calculate list of total precipitation per month (plan: iterate over each item, check if it's
-    #  in a given month, if yes add, if no add to new month)
-    # total_monthly_precipitation = []     # initialize list for monthly precipitation
-    # month = 0                            # initialize month so can iterate over 12 months
-    # while month < 12:                    # for 12 months
-    #     month_precipitation = 0           # initialize variable for total precipitation in a month
-    #     month += 1                       
-    #     for day in seattle_precipitation:
-    #         observation_date = day["date"].split('-')
-    #         observation_month = int(observation_date[1])
-    #         if observation_month == month:    # if day in current month
-    #             month_precipitation += day["value"]    # add day's precipitation to total of that month
-    #     total_monthly_precipitation.append(month_precipitation)   # append to list of all months
-
-    # 1. calculate total monthly precipitation in Seattle
-    # iterate over observations, check month, and make dictionary with key = month, value = total precipitation
-    total_monthly_precipitation = {}
-    for observation in station_precipitation:
+    # 2.3 calculate total monthly precipitation 
+    # make dictionary with key = month, value = total precipitation
+    total_monthly_precipitation = {}                         # initialize dictionary
+    for observation in city_precipitation:                   # each observation contains station, date, precipitation
         observation_date = observation["date"].split('-')
-        observation_month = int(observation_date[1])
+        observation_month = int(observation_date[1])         # access month per observation
         if observation_month not in total_monthly_precipitation:
-            month_precipitation = 0
-        month_precipitation += observation["value"]
-        total_monthly_precipitation[observation_month] = month_precipitation
+            month_precipitation = 0 
+        month_precipitation += observation["value"]          # add up precipitation per month
+        total_monthly_precipitation[observation_month] = month_precipitation      # append to dictionary
 
-    # convert thisdictionary to a list
+    # convert dictionary to a list
     total_monthly_precipitation_list = list(total_monthly_precipitation.values())
 
-    # 2. calculate yearly precipitation using monthly precipitation dictionary
+    # 2.4 calculate yearly precipitation 
     total_yearly_precipitation = 0
-    for observation_month in total_monthly_precipitation:
+    for observation_month in total_monthly_precipitation:        # loop through monthly precipitation dictionary and add values
         month_precipitation = total_monthly_precipitation[observation_month]
         total_yearly_precipitation += month_precipitation
 
-    # 3. calculate relative monthly precipitation (proportion of yearly rain per month)
+    # 2.5 calculate relative monthly precipitation (proportion of yearly rain per month)
     relative_monthly_precipitation = {}
-    for observation_month in total_monthly_precipitation:
-        month_relative_precipitation = total_monthly_precipitation[observation_month]/total_yearly_precipitation
+    for observation_month in total_monthly_precipitation:        # loop through monthly precipitation dictionary 
+        month_relative_precipitation = total_monthly_precipitation[observation_month]/total_yearly_precipitation  # divide by yearly precipitation
         relative_monthly_precipitation[observation_month] = month_relative_precipitation
 
     # convert the dictionary to a list
     relative_monthly_precipitation_list = list(relative_monthly_precipitation.values())
 
-    # add this to existing dictionary
-
-
-    
-    # convert into needed format and write into json file
-    precipitation_summary[city] = {
+    # 2.6 add results to existing dictionary
+    stations_dict[city] = {
     "station": station,
     "state": state,
     "total_monthly_precipitation": total_monthly_precipitation_list,
@@ -105,5 +71,6 @@ for city in stations_dict:
     "relative_yearly_precipitation": 0
     }
 
+# 3. write results to json file
 with open('results.json', 'w', encoding='utf-8') as file:
-    json.dump(precipitation_summary, file, indent=4)
+    json.dump(stations_dict, file, indent=4)
